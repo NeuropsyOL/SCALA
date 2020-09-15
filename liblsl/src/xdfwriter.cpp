fd52ee8c93d827cab8928cf1730d02c82f0f9fd4
s4rify <sarah.blum.sb@gmail.com>
2020-09-15 17:06:03 +0200

    deleted source files from LSL lib and only included .so file

--- liblsl/src/xdfwriter.cpp
+++
@@ -1,7 +0,0 @@
-#include "xdfwriter.h"
-#include "../external/boost/algorithm/string.hpp"
-//#include "../../../android-ndk-r14b/sources/cxx-stl/gnu-libstdc++/4.9/include/iosfwd"
-#include "C:\Users\sarah\AppData\Local\Android\Sdk\ndk\android-ndk-r14b\sources\cxx-stl\gnu-libstdc++\4.9\include\iosfwd"
-#include <iostream>
-
-
